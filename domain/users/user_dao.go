d0e6e8c1d00606aa717dade902b4fcd6b87e36a9
Claudio Petrini <c.petrini@arduino.cc>
2020-07-23 17:26:59 +0200

    Added date_utils, users_db. Connecting to DB

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,7 +2,9 @@ package users
 
 import (
 	"fmt"
+	"github.com/c-petrini/bookstore_users-api/datasources/mysql/users_db"
 
+	"github.com/c-petrini/bookstore_users-api/utils/date_utils"
 	"github.com/c-petrini/bookstore_users-api/utils/errors"
 )
 
@@ -11,6 +13,9 @@ var (
 )
 
 func (user *User) Get(userId int64) *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
@@ -21,7 +26,6 @@ func (user *User) Get(userId int64) *errors.RestErr {
 	user.Email = result.Email
 	user.DateCreated = result.DateCreated
 	return nil
-
 }
 
 func (user *User) Save() *errors.RestErr {
@@ -32,6 +36,7 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
 	usersDB[user.Id] = user
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/c-petrini/bookstore_users-api
 go 1.13
 
 require (
+	github.com/bcmi-labs/arduino-api v0.0.0-20190514102216-db913b48737e // indirect
 	github.com/federicoleon/bookstore_users-api v0.0.0-20200406070935-c8e352aed46b
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/jpillora/ansi v1.0.2 // indirect
 	github.com/jpillora/spy v0.0.0-20150317092452-9295d48fb6db // indirect
